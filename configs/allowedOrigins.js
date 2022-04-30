fae4d71d7d7fc485b98297a1171d8cc65417b800
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-04-30 21:46:47 +1000

    feat: modify config

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
 	'http://127.0.0.1:5500',
 	'http://localhost:3500',
 	'http://localhost:3000',
+	'http://bony-sordid-brochure.glitch.me'
 ];
 
 module.exports = {allowedOrigins}
